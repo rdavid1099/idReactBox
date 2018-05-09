3e0262acfed9e0f8665e469f2d716cfb0c3b4159
Ryan Workman <rdavid1099@gmail.com>
2018-05-09 10:53:51 -0600

    Add bootstrap

--- src/App.js
+++ src/App.js
@@ -8,11 +8,12 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Welcome to IdReactBox ¯\_(ツ)_/¯</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          Where storing your ideas and hitting random API endpoints is our business.
         </p>
+        
       </div>
     );
   }
