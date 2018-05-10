454c251167f2efd13f11b8e031d5c6c630be9ad0
Ryan Workman <rdavid1099@gmail.com>
2018-05-10 14:16:36 -0600

    Add IdeaController#index

--- idreact_box_api/app/controllers/api/v1/idea_controller.rb
+++ idreact_box_api/app/controllers/api/v1/idea_controller.rb
@@ -1,5 +1,8 @@
 class Api::V1::IdeaController < ApplicationController
   def index
+    max = params[:max].to_i > 25 || params[:max].to_i == 0 ? 25 : params[:max]
+    ideas = Idea.where(user_id: params[:user_id]).take(max)
+    render json: ideas
   end
 
   def show
